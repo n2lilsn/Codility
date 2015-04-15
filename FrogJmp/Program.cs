3db62e80516679ef5b454424e5b627776364365b
n2lilsn <n2lilsn@gmail.com>
2015-04-15 11:43:06 +0200

    start

---
+++ FrogJmp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrogJmp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(solution(10,85,30));
+            Console.Read();
+        }
+        static int solution(int X, int Y, int D)
+        {
+            int count = ((Y - X) / D);
+            if (((Y - X) % D) > 0)
+                count++;
+            return count;
+        }
+    }
+}
