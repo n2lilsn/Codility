ec2c3dab07a4c352bee7077f0e63d24f282c21de
n2lilsn <n2lilsn@gmail.com>
2015-05-07 12:35:33 +0200

    END

--- 2.3 MissingInteger/Program.cs
+++ 2.3 MissingInteger/Program.cs
@@ -11,56 +11,27 @@ namespace _2._3_MissingInteger
         static void Main(string[] args)
         {
             int[] A = new int[6];
-            int[] B = new int[2];
-            int[] C = new int[1];
+            A[0] = 1;
+            A[1] = 2;
+            A[2] = 3;
+            A[3] = 4;
+            A[4] = 5;
+            A[5] = 6;
 
-            A[0] = -1;
-            A[1] = -1;
-            A[2] = -1;
-            A[3] = -1000000;
-            A[4] = 0;
-            A[5] = -1;
-
-            B[0] = int.MinValue;
-            B[1] = int.MaxValue;
-
-            C[0] = 2;
-
-            Console.WriteLine(solution(C));
+            Console.WriteLine(solution(A));
             Console.Read();
         }
         public static int solution(int[] A)
         {
+            int min = 1;
             Array.Sort(A);
-            List<int> B = A.ToList();
-            B.RemoveAll(o => o < 0);
-            if (B.Count() != A.Count())
-            {
-                int count = 0;
-                for (int i = 0; i < B.Count; i++)
-                {
-                    if (B[i] == i)
-                        count++;
-                }
-                return count;
-            }
-            else
-            {
-                int p = A[0];
 
-                for (int i = 0; i < A.Length; i++)
-                {
-                    if (A[i] != p)
-                    {
-                        if (A[i] != p + 1 && p >= 0)
-                            return ++p;
-                        else
-                            p++;
-                    }
-                }
+            foreach (var item in A)
+            {
+                if (item > -1 && item == min)
+                    min++;
             }
-
-            return B.Min() - 1;
+            return min;
         }
     }
 }
--- Testy/UnitTest1.cs
+++ Testy/UnitTest1.cs
@@ -10,11 +10,14 @@ namespace Testy
         [TestMethod]
         public void MissingInteger()
         {
+
             var calc = new Program();
 
             int[] A = new int[6];
-            int[] B = new int[6];
-            int[] C = new int[1];
+            int[] B = new int[2];
+            int[] C = new int[7];
+            int[] D = new int[200];
+
             A[0] = 1;
             A[1] = 3;
             A[2] = 6;
@@ -22,10 +25,33 @@ namespace Testy
             A[4] = 1;
             A[5] = 2;
 
-            C[0] = 3;
+            B[0] = int.MinValue;
+            B[1] = int.MaxValue;
 
-            int sol = Program.solution(C);
-            Assert.AreEqual(2, sol);
+            C[0] = -3;
+            C[1] = -2;
+            C[2] = -1;
+            C[3] = 0;
+            C[4] = 1;
+            C[5] = 2;
+            C[6] = 3;
+
+            for (int i = 0; i < 200; i++)
+            {
+                if (i != 101)
+                    D[i] = i;
+            }
+
+            int solA = Program.solution(A);
+            int solB = Program.solution(B);
+            int solC = Program.solution(C);
+            int solD = Program.solution(D);
+
+            Assert.AreEqual(5, solA);
+            Assert.AreEqual(1, solB);
+            Assert.AreEqual(4, solC);
+            Assert.AreEqual(101, solD);
+
         }
     }
 }
